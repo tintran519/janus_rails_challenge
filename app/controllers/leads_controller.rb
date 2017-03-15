85ec5f3fe46bc67241ba710cefcc74269ddf1965
Tin Tran <tin.tran0519@gmail.com>
2017-03-14 23:52:26 -0700

    Yelp client working

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,6 +1,7 @@
 class LeadsController < ApplicationController
   def index
-    @leads = Lead.all
-    @blah = Rails.application.secrets.YELP_TOKEN
+    # @leads = Lead.all
+    parameters = { term: 'food', limit: 2 }
+    @response = Yelp.client.search('Boston', parameters)
   end
 end
---
+++ config/initializers/yelp.rb
@@ -0,0 +1,7 @@
+Yelp.client.configure do |config|
+  config.consumer_key = Rails.application.secrets.yelp_consumer_key
+  config.consumer_secret = Rails.application.secrets.yelp_consumer_secret
+  config.token = Rails.application.secrets.yelp_token
+  config.token_secret = Rails.application.secrets.yelp_token_secret
+end
+
